df05211a5b8068d76ddc43d36f3946251fc52e0b
@anhnv99 <vietanhhappy99@gmail.com>
2023-11-29 15:36:53 +0700

    trello board bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -15,6 +15,7 @@ import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 import { Tooltip } from '@mui/material'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import Profiles from './Menus/Profiles'
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
 
 function AppBar() {
   return (
@@ -55,7 +56,9 @@ function AppBar() {
           <Recent />
           <Starred />
           <Templates />
-          <Button variant="outlined">Create</Button>
+          <Button variant="outlined" startIcon={<LibraryAddIcon />}>
+            Create
+          </Button>
         </Box>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,17 +1,176 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
 
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 function BoardBar() {
   return (
     <Box
       sx={{
-        backgroundColor: 'primary.dark',
         width: '100%',
         height: (theme) => theme.trello.boardBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        overflowX: 'auto',
+        paddingX: 2,
+        borderTop: '1px solid #00bfa5'
       }}
     >
-      Board bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="DevBetter MERN Stack Board"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filter"
+          clickable
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>
+          Invite
+        </Button>
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+          <Tooltip title="devbetter">
+            <Avatar
+              alt="devbetter"
+              src="https://www.devbetter.pro/static/media/happy_learning.ff50107dce7c0d9d2539.jpg"
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
